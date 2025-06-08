d37ba7232462f8577f11aee0ef59814e363f0758
shrutjainn <shrutjain488@gmail.com>
2025-06-09 00:19:52 +0530

    execute workflow file

--- lib/workflow/executeWorkflow.ts
+++ lib/workflow/executeWorkflow.ts
@@ -1,3 +1,10 @@
+import axios from "axios";
 import "server-only";
 
-export async function ExecuteWorkflow(executionId: string) {}
+export async function ExecuteWorkflow(executionId: string) {
+  const environment = { phases: {} };
+
+  const { data } = await axios.post(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/workflow-execution/execute/${executionId}`
+  );
+}
